ec8c12cf1fddda13763eebd80b2e1a51fb6539b0
Julianne <juliannelicon@gmail.com>
2020-11-29 13:09:42 -0300

    create shopping cart

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,18 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    const total = products.reduce((accumulator, product) => {
+      const soma = product.price * product.quantity;
+      return accumulator + soma;
+    }, 0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const quantity = products.reduce((accumulator, product) => {
+      return accumulator + product.quantity;
+    }, 0);
+    return quantity;
   }, [products]);
 
   return (
